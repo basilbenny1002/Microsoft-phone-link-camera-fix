97a76477f976c3bc0783c422de6d5f7b062c562b
basilbenny1002 <basilbenny1002@gmail.com>
2026-01-30 17:00:11 +0530

    Add a conditional to proceed or quit the operation. Made the app repair as teh first step in the sequence

--- FixPhoneLinkCamera.bat
+++ FixPhoneLinkCamera.bat
@@ -1,41 +1,70 @@
 @echo off
-:: Automatically request Admin rights
+setlocal enabledelayedexpansion
+
+:: 1. Automatically request Admin rights
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>&1 || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-echo ===========================================
-echo   Fixing Phone Link Camera Connection...
-echo ===========================================
+:menu
+cls
+echo ===================================================
+echo           PHONE LINK CONNECTION REPAIR
+echo ===================================================
+echo.
+echo This script will perform the following actions:
+echo   1) Repair the Phone Link App (via PowerShell)
+echo   2) Restart the Bluetooth User Support Service
+echo   3) Restart the Phone Service
+echo.
+echo Note: These steps can also be performed manually 
+echo in Windows Settings and the Services manager.
+echo.
 
-:: 1. Restart Phone Service
-echo Stopping Phone Service...
-net stop PhoneSvc /y
-echo Starting Phone Service...
-net start PhoneSvc
-echo Phone service started
+:choice
+set /P c=Do you want to proceed? [Y/N]: 
+if /I "%c%" EQU "Y" goto :proceed
+if /I "%c%" EQU "N" goto :quit
+echo Invalid input, please type Y or N.
+goto :choice
 
-:: 2. Restart Bluetooth User Support Service
-echo Searching for Bluetooth User Support Service...
-:: Searching for the specific name, since the names are dynamic
+:proceed
+echo.
+echo [1/3] Closing Phone Link and Repairing the App...
+taskkill /f /im PhoneExperienceHost.exe >nul 2>&1
+powershell -command "Get-AppxPackage *Microsoft.YourPhone* | Reset-AppxPackage"
+echo [OK] Phone Link app repair complete.
+
+echo.
+echo [2/3] Searching for Bluetooth User Support Service...
+:: Since the service name is dynamic and varies from different machines
 for /f "tokens=1" %%a in ('wmic service where "name like 'BluetoothUserService_%%'" get name ^| findstr /i "BluetoothUserService"') do set "BT_SERVICE=%%a"
 
 if defined BT_SERVICE (
-    echo Stopping %BT_SERVICE%...
-    net stop %BT_SERVICE% /y
-    echo Starting %BT_SERVICE%...
+    echo Restarting %BT_SERVICE%...
+    net stop %BT_SERVICE% /y >nul 2>&1
     net start %BT_SERVICE%
+    echo [OK] Bluetooth Service restarted.
 ) else (
-    echo [!] Could not find Bluetooth User Support Service.
+    echo [!] Could not find a dynamic Bluetooth User Support Service.
 )
 
-:: Repair teh app
-::Kill the app process first
-echo Closing Phone Link processes...
-taskkill /f /im PhoneExperienceHost.exe >nul 2>&1
-echo Triggering App Repair for Phone Link...
-powershell -command "Get-AppxPackage *Microsoft.YourPhone* | Reset-AppxPackage"
-echo Phone Link app repair complete
+echo.
+echo [3/3] Restarting Phone Service...
+net stop PhoneSvc /y >nul 2>&1
+net start PhoneSvc
+echo [OK] Phone Service restarted.
 
 echo.
-echo All services have been restarted and the Phone Link app has been repaired. Try opening Phone Link now!
-pause
+echo ===================================================
+echo DONE! Please try opening Phone Link now.
+echo ===================================================
+echo This window will close automatically in 5 seconds...
+timeout /t 5 >nul
+exit
+
+:quit
+echo.
+echo Operation cancelled by user. 
+echo Closing in 3 seconds...
+timeout /t 3 >nul
+exit
